77472c941048cedf5724877a041735c7f460d775
vsr94539 <v.shekhar@yahoo.com>
2020-10-03 14:31:50 -0700

    Minor Edits and cleanups

--- ec2-cpu-snapshot.py
+++ ec2-cpu-snapshot.py
@@ -1,6 +1,14 @@
 import json
 import boto3
 import datetime as datetime
+bucket_name = "137965528627-ec2-rightsizing"
+def s3_load_data (item, current_date):
+    objname = 'ec2-cpu'+ "-" + current_date
+    s3 = boto3.resource('s3')
+    obj = s3.Object(bucket_name, objname)
+    print("####### Going to upload data to bucket:", bucket_name )
+    print("####### Going to upload following data to the bucket:", item)
+    obj.put(Body=json.dumps(item))
 # Get list of regions
 ec2_client = boto3.client("ec2")
 regions = [region['RegionName']
@@ -8,10 +16,13 @@ regions = [region['RegionName']
 print ("######### List of all regions to be iterated:")
 print(regions)
 current_utc_time = datetime.datetime.utcnow()
+current_date = current_utc_time.isoformat()
 past_utc_time = datetime.datetime.utcnow() - datetime.timedelta(hours=24)
 print("######### Metric Start Time:", past_utc_time)
 print("######### Metric End Time:",current_utc_time)
-regions = ["us-west-2"]
+#regions = ["us-west-2"]
+datapoints_all = []
+#regions = ['us-west-2']
 for region in regions:
     cloudwatch = boto3.resource('cloudwatch', region)
     metric = cloudwatch.Metric('AWS/EC2', 'CPUUtilization')
@@ -26,6 +37,7 @@ for region in regions:
                   'Values': ['running']}])
     print(type(instances))
 
+    datapoints_all_json = {}
     for instance in instances:
         print (instance.id)
         metric_response_av_cpu = metric.get_statistics(
@@ -56,20 +68,18 @@ for region in regions:
                 'Maximum',
             ]
         )
-        datapoints_av_cpu = metric_response_av_cpu.get("Datapoints")
-        datapoints_mx_cpu = metric_response_mx_cpu.get("Datapoints")
-        datapoints_av_cpu = datapoints_av_cpu[0]
-        datapoints_mx_cpu = datapoints_mx_cpu[0]
-        datapoints_all = {**datapoints_av_cpu, **datapoints_mx_cpu}
-        print(datapoints_all)
-        #print (datapoints_mx_cpu)
-        #metric_response_mx_cpu['InstanceId'] = instance.id
-        #print (metric_response_composite)
-        #print ((metric_response_av_cpu))
-        #print ((metric_response_mx_cpu))
-        jout = (json.dumps(datapoints_all, default=str))
-        #jout["instance.id"] = instance.id
-        print(jout)
+        instance_datapoints_av_cpu = metric_response_av_cpu.get("Datapoints")
+        instance_datapoints_mx_cpu = metric_response_mx_cpu.get("Datapoints")
+        instance_datapoints_av_cpu = instance_datapoints_av_cpu[0]
+        instance_datapoints_mx_cpu = instance_datapoints_mx_cpu[0]
+        instance_datapoints_all = {**instance_datapoints_av_cpu, **instance_datapoints_mx_cpu}
+        instance_datapoints_all['InstanceId'] = instance.id
+        print(instance_datapoints_all)
         all_instance_ids.append(instance.id)
         other_instance_ids = set(all_instance_ids) - (set(test_instance_ids))
+        datapoints_all.append(instance_datapoints_all)
     print('All Instances in region:', region, all_instance_ids)
+print('Printing all datapoints: ', datapoints_all)
+datapoints_all_json = (json.dumps(datapoints_all,default=str))
+print('Printing all datapoints JSON: ', datapoints_all_json)
+s3_load_data(datapoints_all_json, current_date)
