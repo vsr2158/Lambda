ec67a4a3fe3e4827217aee8bf81d6a6ec66ae45b
vsr94539 <v.shekhar@yahoo.com>
2020-10-03 12:51:28 -0700

    Minor Edits and cleanups

--- ec2-cpu-snapshot.py
+++ ec2-cpu-snapshot.py
@@ -1,3 +1,4 @@
+import json
 import boto3
 import datetime as datetime
 # Get list of regions
@@ -10,6 +11,7 @@ current_utc_time = datetime.datetime.utcnow()
 past_utc_time = datetime.datetime.utcnow() - datetime.timedelta(hours=24)
 print("######### Metric Start Time:", past_utc_time)
 print("######### Metric End Time:",current_utc_time)
+regions = ["us-west-2"]
 for region in regions:
     cloudwatch = boto3.resource('cloudwatch', region)
     metric = cloudwatch.Metric('AWS/EC2', 'CPUUtilization')
@@ -26,7 +28,7 @@ for region in regions:
 
     for instance in instances:
         print (instance.id)
-        metric_response = metric.get_statistics(
+        metric_response_av_cpu = metric.get_statistics(
             Dimensions=[
                 {
                     'Name': 'InstanceId',
@@ -40,7 +42,34 @@ for region in regions:
                 'Average',
             ]
         )
-        print (metric_response)
+        metric_response_mx_cpu = metric.get_statistics(
+            Dimensions=[
+                {
+                    'Name': 'InstanceId',
+                    'Value': instance.id
+                },
+            ],
+            StartTime=past_utc_time,
+            EndTime=current_utc_time,
+            Period=86400,
+            Statistics=[
+                'Maximum',
+            ]
+        )
+        datapoints_av_cpu = metric_response_av_cpu.get("Datapoints")
+        datapoints_mx_cpu = metric_response_mx_cpu.get("Datapoints")
+        datapoints_av_cpu = datapoints_av_cpu[0]
+        datapoints_mx_cpu = datapoints_mx_cpu[0]
+        datapoints_all = {**datapoints_av_cpu, **datapoints_mx_cpu}
+        print(datapoints_all)
+        #print (datapoints_mx_cpu)
+        #metric_response_mx_cpu['InstanceId'] = instance.id
+        #print (metric_response_composite)
+        #print ((metric_response_av_cpu))
+        #print ((metric_response_mx_cpu))
+        jout = (json.dumps(datapoints_all, default=str))
+        #jout["instance.id"] = instance.id
+        print(jout)
         all_instance_ids.append(instance.id)
         other_instance_ids = set(all_instance_ids) - (set(test_instance_ids))
     print('All Instances in region:', region, all_instance_ids)
